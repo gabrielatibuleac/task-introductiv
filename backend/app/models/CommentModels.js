cdf05338436558063ce15c14aab73ccff3a8d490
Cata69 <catalin.rusu884@gmail.com>
2025-05-08 14:59:23 +0300

    create comment model

--- backend/app/models/CommentModels.js
+++ backend/app/models/CommentModels.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 
-const ItemSchema = new mongoose.Schema({
-    name: {
+const CommentSchema = new mongoose.Schema({
+    
+    content: {
         type: String,
-        required: true
+        required: [true, 'Adauga comentariu'],
+        maxlength: [1000, 'Nu poate trece de 1000 de caractere']
+    },
+    
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    
+    userName: {
+        type: String,
+        required: [true, 'Adauga numele utilizatorului']
     }
 });
 
-module.exports = mongoose.model('Item', ItemSchema);
+module.exports = mongoose.model('Comment', CommentSchema);
