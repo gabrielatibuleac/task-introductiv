d36f27f0938b92ebb1358189c27c76b5013f4618
gabrielatibuleac <gabrielatibuleac344@gmail.com>
2025-05-09 17:25:17 +0300

    Am facut inceputul la prima pagina

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { MembersDetailsComponent } from './pages/members-details/members-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent
+    },
+    { path:'member/:id', 
+        component: MembersDetailsComponent
+    },
+
+
+];
---
+++ frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent implements OnInit {
+  animatedText = '';
+  welcomeMessage = 'Haide să cunoaștem echipa!';
+  showTeam = false;
+  logoPath = 'assets/images/asii.png'; 
+    confettiParticles: any[] = [];
+  ngOnInit(): void {
+  this.typeWriterEffect(); // nu mai apelăm generateConfetti aici
+}
+
+typeWriterEffect(): void {
+  this.generateConfetti(150);
+
+  let i = 0;
+  const typing = () => {
+    if (i < this.welcomeMessage.length) {
+      this.animatedText += this.welcomeMessage.charAt(i);
+      i++;
+      setTimeout(typing, 100);
+    } else {
+      setTimeout(() => this.showTeam = true, 500);
+    }
+  };
+  typing();
+}
+
+
+  generateConfetti(count:number) {
+    const colors = ['#ff0', '#f0f', '#0ff', '#f00', '#0f0', '#00f'];
+   this.confettiParticles = Array(count).fill(0).map((_, i) => ({
+    
+      id: i,
+      left: Math.random() * 100 + 'vw',
+      animationDuration: (Math.random() * 3 + 2) + 's',
+      animationDelay: (Math.random() * 5) + 's',
+      background: colors[Math.floor(Math.random() * colors.length)],
+      size: (Math.random() * 10 + 5) + 'px',
+      shape: Math.random() > 0.5 ? 'square' : 'circle'  
+      
+  }));
+  }
+}
